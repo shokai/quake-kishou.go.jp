dac246cadd6eaa0380c91bf38a999af88641bc2f
Sho Hashimoto <hashimoto@shokai.org>
2012-02-26 01:00:55 +0900

    add dump_by_date.rb - dump quakes specified date

---
+++ bin/dump_by_date.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+## dump quake data specified date
+
+require File.dirname(__FILE__)+'/../bootstrap'
+
+date = ARGV.empty? ? Date.today-1 : Date.parse(ARGV.shift)
+
+Quake.find_by_date(date).asc(:time).each do |q|
+  puts "#{q.place} #{q.time} - magnitude:#{q.magnitude}, depth:#{q.depth}"
+end
--- models/quake.rb
+++ models/quake.rb
@@ -10,4 +10,18 @@ class Quake
   def time
     self[:time].getlocal
   end
+
+  def self.find_by_date(date)
+    where(:time.gt => date, :time.lt => date+1)
+  end
+
+  def to_json
+    {
+      :place => place,
+      :time => time,
+      :point => point,
+      :depth => depth,
+      :magnitude => magnitude
+    }.to_json
+  end
 end
